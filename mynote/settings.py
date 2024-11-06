4d3640ab479575f1f83df259fa4a02727ed9f426
viruss001 <122778544+viruss001@users.noreply.github.com>
2024-11-06 14:48:28 +0530

    last steps

--- mynote/settings.py
+++ mynote/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 import os
 STATIC_URL = '/assets/'
+STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
 #VITE_APP_DIR = BASE_DIR / "src"
 STATICFILES_DIRS = [
     BASE_DIR / "fronted/dist/assets",
--- mynote/urls.py
+++ mynote/urls.py
@@ -19,11 +19,16 @@ from django.urls import path,include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include("api.url")),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
-urlpatterns += static(settings.MEDIA_URL,
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
                               document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
